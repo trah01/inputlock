2010c0d7f831b8867d06acdf43bef7abb3fccc19
王诗超 <scww@live.com>
2025-12-19 21:49:22 +0800

    Initial Commit

---
+++ lockinput/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  lockinput
+//
+//  Created by dave on 2025/12/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ lockinput/lockinputApp.swift
@@ -0,0 +1,17 @@
+//
+//  lockinputApp.swift
+//  lockinput
+//
+//  Created by dave on 2025/12/19.
+//
+
+import SwiftUI
+
+@main
+struct lockinputApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
